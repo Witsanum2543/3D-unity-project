a27ac7710bce1f1c5a5233f09aa9c1b13b82168c
Witsanu <witsanum2543@hotmail.com>
2023-03-11 17:50:01 +0700

    fixed player constatly floating up when play animation (Bake Into Pose)

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -12,13 +12,13 @@ public class PlayerController : MonoBehaviour
     // Receive game input to handle it
     [SerializeField] private GameInput gameInput;
     // controller that move our player's character
-    private CharacterController controller;
+    private CharacterController characterController;
 
     // Animation state
-    private bool isRunning;
+    private bool isRunning = false;
 
     private void Start() {
-        controller = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update() {
@@ -29,16 +29,32 @@ public class PlayerController : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        handleGravity(moveDirection); 
+
         Vector3 velocity = moveSpeed * moveDirection * Time.deltaTime ;
 
         if (moveDirection.magnitude >= 0.1f) {
             transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
 
-            controller.Move(velocity);
+            characterController.Move(velocity);
         }
 
         isRunning = moveDirection != Vector3.zero;
     }
+
+    void handleGravity(Vector3 moveDirection) {
+        if (characterController.isGrounded) {
+            Debug.Log("GDS");
+            float groundedGravity = -.05f;
+            moveDirection.y = groundedGravity;
+        } else {
+            // Debug.Log("123");
+            float gravity = -9.8f;
+            moveDirection.y = gravity;
+        }
+    }
+
+    // ************************** ANIMATION SECTION ***********************************************
 
     public bool IsRunning() {
         return isRunning;
